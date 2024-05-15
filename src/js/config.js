8f50cb37422d99eebd065a082b3c51d82c4393f1
Rishit <rishit1910kumar@gmail.com>
2024-05-15 14:37:58 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "1b58e0d0-c9e0-4e63-8939-513e7ef5ffbc";
+export const MODAL_CLOSE_SEC = 2.5;
